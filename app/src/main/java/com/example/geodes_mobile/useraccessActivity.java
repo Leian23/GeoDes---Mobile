24c83ab8cf15610a2dd83432cb18ae9666425542
YuehanKaichi <dichosojohncarlo@gmail.com>
2023-09-18 23:22:09 +0800

    try the textboxes

--- app/src/main/java/com/example/geodes_mobile/useraccessActivity.java
+++ app/src/main/java/com/example/geodes_mobile/useraccessActivity.java
@@ -1,16 +1,45 @@
 package com.example.geodes_mobile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class useraccessActivity extends AppCompatActivity {
-
+    public void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // You can add code here to handle the OK button click
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useraccess);
+
+        EditText uName = findViewById(R.id.userName);
+        EditText uPass = findViewById(R.id.userPass);
+
+        String userName = uName.getText().toString();
+        String userPass = uPass.getText().toString();
+
+        if (userName == "admin" && userPass == "1234")
+        {
+            showMessage("Alert", "Correct! ");
+        }
+        else
+        {
+            showMessage("Alert","Wrong!");
+        }
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
