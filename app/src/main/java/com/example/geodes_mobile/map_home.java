d649d011e064d47b0bead0549d999c06f8bc0bed
YuehanKaichi <dichosojohncarlo@gmail.com>
2023-09-26 01:44:11 +0800

    Add zoom and rotation of map

--- app/src/main/java/com/example/geodes_mobile/map_home.java
+++ app/src/main/java/com/example/geodes_mobile/map_home.java
@@ -257,7 +257,7 @@ public class map_home extends AppCompatActivity {
     }
 
     // Method to add inner and outer circles with the user's location as the center
-    private void addLocationCircles() {
+    /*private void addLocationCircles() {
         // Get the user's last known location
         Location lastKnownLocation = myLocationOverlay.getLastFix();
         if (lastKnownLocation != null) {
@@ -318,7 +318,7 @@ public class map_home extends AppCompatActivity {
         mapView.getOverlayManager().add(polyline);
 
         return circle;
-    }
+    }*/
     @Override
     protected void onResume() {
         super.onResume();
